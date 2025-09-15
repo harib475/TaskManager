3d534aae8c941528c18d44e1a6e8e1a30bf2859c
MAC <mac@MACs-MacBook-Pro.local>
2025-09-15 21:04:08 +0500

    add all

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # This allows frontend (React, etc.) to talk with backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://task-manager-cblxbvosl-haribs-projects-d3ee50f2.vercel.app/"],  # ✅ Use your frontend origin
+    allow_origin_regex=r"https:\/\/.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
