fbc47b625d5fa2e480cb8ff69c7d6386270b6c9b
MAC <mac@MACs-MacBook-Pro.local>
2025-09-15 19:11:53 +0500

    add vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  VITE_API_URL: process.env.VITE_API_URL || "https://backend-oghbce1uy-haribs-projects-d3ee50f2.vercel.app/",
+  VITE_SOCKET_URL: process.env.VITE_SOCKET_URL || "https://backend-oghbce1uy-haribs-projects-d3ee50f2.vercel.app/ws/tasks"
 })
